146fc47c20b3ad8d4184ac34250f3eb20cc40dde
deneska333 <nasybulindenis@gmail.com>
2025-03-10 17:56:02 +0300

    Fix Url's

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -21,9 +21,9 @@ func main() {
 	handler := handlers.NewHandler(service)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/messages", handler.PostTaskHandler).Methods("POST")
-	router.HandleFunc("/api/messages", handler.GetTaskHandler).Methods("GET")
-	router.HandleFunc("/api/messages/{id}", handler.PatchTaskHandler).Methods("PATCH")
-	router.HandleFunc("/api/messages/{id}", handler.DeleteTaskHandler).Methods("DELETE")
+	router.HandleFunc("/api/task", handler.PostTaskHandler).Methods("POST")
+	router.HandleFunc("/api/task", handler.GetTaskHandler).Methods("GET")
+	router.HandleFunc("/api/task/{id}", handler.PatchTaskHandler).Methods("PATCH")
+	router.HandleFunc("/api/task/{id}", handler.DeleteTaskHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
